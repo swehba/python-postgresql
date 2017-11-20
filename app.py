205cdf426768c243509fc381f53d3eb0559eab8d
Steve Wehba <swehba@blastmotion.com>
2017-11-19 18:13:46 -0800

    Lesson 45

---
+++ app.py
@@ -0,0 +1,4 @@
+from movie import Movie
+
+movie = Movie("The Matrix", "sci-fi")
+print(movie.name)
---
+++ movie.py
@@ -0,0 +1,7 @@
+class Movie:
+    def __init__(self, name, genre):
+        self.name = name
+        self.genre = genre
+
+    def __repr__(self):
+        return f'<Movie "{movle.name}"'
